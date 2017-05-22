bea8ada40ea84a096710fe7bad9a8896447bba20
Cameron Jensen and Keith Stedman <cameron@>
2017-05-22 13:56:11 -0700

    added custom fonts and caped letters

--- app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
@@ -8,17 +8,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Typeface;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+            Typeface solo = Typeface.createFromAsset(getAssets(), "fonts/champ2.ttf");
+            mAppNameTextView.setTypeface(solo);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
 
