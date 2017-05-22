ae9b70beb02e6a0e2291887916939f371a3a7f7f
Cameron Jensen and Keith Stedman <cameron@>
2017-05-22 13:32:57 -0700

    init setup and first steps with testing

---
+++ app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.guest.myrestaurants;
+
+import android.content.Intent;
+import android.support.annotation.MainThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantsButton);
+
+        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
+    }
+}
