33db62ed0636cc25c16e83707e2f2a9369f185d7
Cameron Jensen <jensenseanc@gmail.com>
2017-06-06 20:25:38 -0700

    add logout in overflow menu

--- app/src/main/java/com/example/guest/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,7 @@ import android.graphics.Typeface;
 
 import com.example.guest.myrestaurants.Constants;
 import com.example.guest.myrestaurants.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,47 +28,17 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//    private SharedPreferences mSharedPreferences;
-//    private SharedPreferences.Editor mEditor;
-
-//    private DatabaseReference mSearchedLocationReference;
-//    private ValueEventListener mSearchedLocationReferenceListener;
 
     @Bind (R.id.findRestaurantsButton) Button mFindRestaurantButton;
     @Bind(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
-//    @Bind (R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        mSearchedLocationReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-
-//        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for(DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
-//                    String location = locationSnapshot.getValue().toString();
-//                    Log.d("Locations updated", "location: " + location);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor = mSharedPreferences.edit();
 
         Typeface solo = Typeface.createFromAsset(getAssets(), "fonts/champ2.ttf");
         mAppNameTextView.setTypeface(solo);
@@ -76,13 +50,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         if(v == mFindRestaurantButton) {
-//            String location = mLocationEditText.getText().toString();
-//            saveLocationToFirebase(location);
-//            if(!(location).equals("")) {
-//                addToSharedPreferences(location);
-//            }
+
             Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-//            intent.putExtra("location", location);
             startActivity(intent);
         }
         if(v == mSavedRestaurantsButton) {
@@ -92,17 +61,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-//    public void saveLocationToFirebase(String location) {
-//        mSearchedLocationReference.push().setValue(location);
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-//    @Override
-//    protected void onDestory() {
-//        super.onDestroy();
-//        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-//    private void addToSharedPreferences(String location) {
-//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-//    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 }
