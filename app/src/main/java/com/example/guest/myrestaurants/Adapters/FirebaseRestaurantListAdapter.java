08d1ac0daed2df885e68774c4cb2e28b2f4772f6
Cameron Jensen <jensenseanc@gmail.com>
2017-06-10 13:52:37 -0700

    items can be moved and deleted

--- app/src/main/java/com/example/guest/myrestaurants/Adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/example/guest/myrestaurants/Adapters/FirebaseRestaurantListAdapter.java
@@ -41,11 +41,12 @@ public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<Resta
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
+        notifyItemMoved(fromPosition, toPosition);
         return false;
     }
 
     @Override
     public void onItemDismiss(int position) {
-
+        getRef(position).removeValue();
     }
 }
